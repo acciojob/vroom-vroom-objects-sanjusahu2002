5fee17d32266525af38af39e611aa8aae4cbf347
sanjusahu2002 <132829320+sanjusahu2002@users.noreply.github.com>
2024-11-11 10:19:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	this.make = make;
+    this.model = model;
+}
+Car.prototype.getMakeModel = function(){
+    return `${this.make} ${this.model}`;
+};
 
-function SportsCar(make, model, topSpeed) {}
+function SportsCar(make, model, topSpeed) {
+	Car.call(this,make,model);// it inherits property from car
+    this.topSpeed = topSpeed;
+}
+SportsCar.prototype = Object.create(Car.prototype); // inherits car prototype in sportscar prototype.
+SportsCar.prototype.constructor = SportsCar;
 
+SportsCar.prototype.getTopSpeed = function (){
+            return `${this.topSpeed}`;
+}
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
